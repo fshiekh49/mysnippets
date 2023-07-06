fdb9962f50f7c2f950bbb8108617c8f48ec3009a
Faheem <mohammedfahim840@gmail.com>
2023-07-06 23:42:23 +0530

    Initial commit

---
+++ test333.py
@@ -0,0 +1,21 @@
+import pyautogui
+# print(pyautogui.position())
+
+
+
+
+running = True
+
+while running:
+
+    pyautogui.moveTo(82, 17, duration=2)
+
+    pyautogui.click()
+
+    pyautogui.moveTo(105, 55, duration=2)
+
+    pyautogui.click()
+
+    pyautogui.moveTo(411, 75, duration=2)
+
+    pyautogui.click()
